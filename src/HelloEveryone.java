266ae5da076406fc22a6287b040e75ed5928f030
SyberSky <60616@mail.ru>
2018-03-02 20:29:16 +0300

    asd

---
+++ src/HelloEveryone.java
@@ -0,0 +1,9 @@
+/**
+ * Created by sybersky on 02.03.2018.
+ */
+public class HelloEveryone {
+
+    public static void main(String[] args) {
+        System.out.println("asdasdasd");
+    }
+}
---
+++ src/general/AsMain.java
@@ -0,0 +1,10 @@
+package general;
+
+/**
+ * Created by SyberSky on 02.03.2018.
+ */
+public class AsMain {
+    public static void main(String[] args) {
+
+    }
+}
